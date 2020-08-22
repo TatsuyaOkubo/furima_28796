0d0a3ef36116a9b53e8d2f3afdb04bb291fef894
TatsuyaOkubo <sa122moshita@gmail.com>
2020-08-22 20:04:44 +0900

    ユーザー情報のバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+
+  validates :password, presence: true, length: {minimum: 6}, format: { with: /\A[a-zA-Z0-9]+\z/ }
+
+  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  
+  validates :last_name_kana, :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+
+  validates :birthday, presence: true
+
 end

5c10e346f8593c79b808b5a1049464ce201f0e1f
TatsuyaOkubo <sa122moshita@gmail.com>
2020-08-27 18:12:35 +0900

    出品した商品の一覧表示

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,15 @@
+class PurchasesController < ApplicationController
+
+  def new
+    @purchase = Purchase.new
+  end
+
+  def create
+    @purchase = Purchase.new(purchase_params)
+    if @purchase.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "products#index"
   resources :products, only: [:index, :new, :create]
+  resources :phurchase, only: [:new, :create]
 end

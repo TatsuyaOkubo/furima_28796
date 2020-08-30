50dce41d93947ef2d484d19faec1af42cd8c85c2
TatsuyaOkubo <sa122moshita@gmail.com>
2020-08-30 14:29:19 +0900

    pay.jpのgem導入

--- db/migrate/20200827062405_create_purchases.rb
+++ db/migrate/20200827062405_create_purchases.rb
@@ -1,8 +1,8 @@
 class CreatePurchases < ActiveRecord::Migration[6.0]
   def change
     create_table :purchases do |t|
-      t.integer :user_id
-      t.integer :product_id
+      t.integer :user_id    ,null: false
+      t.integer :product_id ,null: false
       t.timestamps
     end
   end

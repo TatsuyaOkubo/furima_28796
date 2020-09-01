108e00f1b416871b11b4578c572a7eb91a241c81
TatsuyaOkubo <sa122moshita@gmail.com>
2020-09-01 17:58:29 +0900

    Revert "購入機能の実装"
    This reverts commit f27dd54a2c55eeb3e19ecc788162afa7f5e97622.

--- app/forms/purchase_receiver.rb
+++ app/forms/purchase_receiver.rb
@@ -1,17 +1,21 @@
 class PurchaseReceiver
 
   include ActiveModel::Model
-  attr_accessor :user_id, :product_id, :postcode, :prefecture_id, :city, :block, :building, :phone_number, :purchase_id, :token
+  attr_accessor :name, :user_id, :product_id, :postcode, :prefecture_id, :city, :block, :building, :phone_number, :purchase_id
+
+  validates :user_id, :product_id, presence: true
 
   with_options presence: true do
-    validates :user_id, :product_id, :city, :block, :token
+    validates :user_id, :product_id
     validates :postcode, format: { with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Input half-width characters."}
+    validates :city, :block, format: { message: "is invalid."}
     validates :phone_number, format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width numbers."}
     validates :prefecture_id, numericality: { other_than: 1 }
   end
+  validates :building { message: "is invalid."}
 
   def save
     purchase = Purchase.create(user_id: user_id, product_id: product_id)
-    Receiver.create(postcode: postcode, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number, purchase_id: purchase.id)
+    Receiver.create(postcode: postcode, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number, purchase_id: purchase_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "products#index"
   resources :products do
-    resources :purchases, only: [:index, :new, :create]
+    resources :purchases, only: [:index, :create]
   end
+  resources :receivers, only: [:create]
 end

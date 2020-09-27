654999166d88af9c6efc73c92bc19de09c75e466
TatsuyaOkubo <sa122moshita@gmail.com>
2020-09-27 19:07:52 +0900

    編集ページでの画像プレビュー表示

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,3 +1,4 @@
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
 document.addEventListener('DOMContentLoaded', function(){
   const ImageList = document.getElementById('image-list');
   document.getElementById('item-image').addEventListener('change', function(e){
@@ -5,8 +6,8 @@ document.addEventListener('DOMContentLoaded', function(){
     if (imageContent){
       imageContent.remove();
     }
-    const file = e.target.files[0];
-    const blob = window.URL.createObjectURL(file);
+    
+    const createImageHTML = (blob) => {
     const imageElement = document.createElement('div');
     const blobImage = document.createElement('img');
     blobImage.setAttribute('src', blob);
@@ -14,5 +15,12 @@ document.addEventListener('DOMContentLoaded', function(){
     blobImage.height = 250;
     imageElement.appendChild(blobImage);
     ImageList.appendChild(imageElement);
+    };
+
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+
+    createImageHTML(blob);
   });
 });
+}
